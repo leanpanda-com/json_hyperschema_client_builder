4438de37c5f4ca83439de3907296d609fceda004
Joe Yates <joe.g.yates@gmail.com>
2016-08-25 17:14:29 +0200

    Tweaks for release

--- mix.exs
+++ mix.exs
@@ -1,14 +1,19 @@
 defmodule JSONHyperschema.ClientBuilder.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :json_hyperschema_client_builder,
-      version: "0.1.0",
+      version: @version,
+      description: "Generate HTTP clients based on JSON Hyperschemas",
       elixir: "~> 1.3",
+      package: package,
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps,
+      contributors: ["Joe Yates"],
     ]
   end
 
@@ -16,8 +21,17 @@ defmodule JSONHyperschema.ClientBuilder.Mixfile do
     [applications: []]
   end
 
+  defp package do
+    %{
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/cantierecreativo/json_hyperschema_client_builder"},
+      maintainers: ["Joe Yates"]
+    }
+  end
+
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:ex_json_schema, "~> 0.5.1"},
       {:httpotion, "~> 3.0.0"},
       {:json, "~> 0.3.3"}
